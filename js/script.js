1ab287e9d5c2a32de1d45940902b8effeedcf5a4
Mariuskvaal <mariuskvaal1@gmail.com>
2022-06-15 11:02:48 +0200

    Question solving
    Done with question 1 to 5

--- js/script.js
+++ js/script.js
@@ -12,3 +12,43 @@ const cats = [
     }
 ];
 
+
+//question 1
+const cat = {
+    complain: function() {
+        console.log("Meow!")
+    }
+}
+
+console.log(cat.complain);
+
+
+
+//question 2
+
+const heading = document.querySelector ("h3");
+heading.innerHTML = "updatet heading";
+
+
+// question 3
+
+heading.style.fontSize = "2em";
+
+// question 4 
+
+console.dir(heading.className);
+
+heading.className = "subheading";
+
+console.log(heading.className)
+
+// question 5
+
+const paragraphs = document.querySelectorAll("p");
+
+for(let i = 0; i < paragraphs.length; i++) {
+
+paragraphs[i].style.color = "red";
+
+}
+
